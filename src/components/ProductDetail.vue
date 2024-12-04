ef8fc5ad27bed08d2f1c4ca5817823c5c5ef4329
Khanh2812 <khanhtbph53485@gmail.com>
2024-12-04 10:23:28 +0700

    add file

---
+++ src/components/ProductDetail.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const id = '1';
+const urlDetailProduct = 'https://67414054e4647499008d305b.mockapi.io/api/v1/products/' + id;
+const product = ref({})
+
+async function getProduct() {
+  product.value = await (await fetch(urlDetailProduct)).json();
+}
+getProduct();
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ product.name }} - {{ product.id }}</h1>
+    <p>Quantity: {{ product.quantity }}</p>
+    <p>Price: {{ product.price }}</p>
+    <p>{{ product.description }}</p>
+  </div>
+</template>
---
+++ src/router.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ProductList from './components/ProductList.vue'
+import CategoryList from './components/CategoryList.vue'
+import ProductDetail from './components/ProductDetail.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Default',
+    component: ProductList,
+  },
+  {
+    path: '/products',
+    name: 'ProductList',
+    component: ProductList,
+  },
+  {
+    path: '/categories',
+    name: 'CategoryList',
+    component: CategoryList,
+  },
+  {
+    path: '/product/1',
+    name: 'ProductDetail',
+    component: ProductDetail,
+  }
+  
+
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
